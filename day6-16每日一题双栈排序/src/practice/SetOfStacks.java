4dc9a1a9e62e32d1fdb0f4181271e33d016d5463
logicr <13314433445@163.com>
2018-06-17 19:08:09 +0800

    add practice code JDBC

--- day6-16每日一题双栈排序/src/practice/SetOfStacks.java
+++ day6-16每日一题双栈排序/src/practice/SetOfStacks.java
@@ -1,13 +1,10 @@
 package practice;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Stack;
-import java.util.stream.Collectors;
 
 /**
  * 集合栈，已经通过编译
- * 
+ *
  */
 //
 //public class SetOfStacks {
@@ -52,28 +49,27 @@ public class SetOfStacks {
     public ArrayList<ArrayList<Integer>> setOfStacks(int[][] ope, int size) {
         ArrayList<ArrayList <Integer>> list = new ArrayList<ArrayList<Integer>>();
         ArrayList<Integer> stack = null;
-        for (int i = 0; i < ope.length; i++) {
-            int ints = ope[i][0];
-            if(1 == ints){
-                /**
+        for (int[] anOpe : ope) {
+            int ints = anOpe[0];
+            if (1 == ints) {
+                /*
                  * 这里用null的判断可以将stack一开始就入list，避免了最后一个stack不能入list
                  */
-                if (null == stack || size == stack.size()){
+                if (null == stack || size == stack.size()) {
 
                     stack = new ArrayList<>();
                     list.add(stack);
                 }
-                stack.add(ope[i][1]);
-            }
-            else {
-                if (stack.isEmpty()){
-                    /**
+                stack.add(anOpe[1]);
+            } else {
+                if (stack.isEmpty()) {
+                    /*
                      * 因为每个Stack都是在list中的，所以取删除后的下一个
                      */
-                    list.remove(list.size()-1);
-                    stack = list.get(list.size()-1);
+                    list.remove(list.size() - 1);
+                    stack = list.get(list.size() - 1);
                 }
-                stack.remove(stack.size()-1);
+                stack.remove(stack.size() - 1);
             }
         }
         return list;
---
+++ day6-17jdbc/src/jdbcdemo/JdbcDemo.java
@@ -0,0 +1,57 @@
+package jdbcdemo;
+
+import java.sql.*;
+import java.util.Collections;
+
+public class JdbcDemo {
+    public static void jdbcStep(){
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+//            加载类驱动
+            Class.forName("com.mysql.jdbc.Driver");
+//            连接数据库
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/scott?user=root&password=147258&useSSL = true");
+//           创建命令
+//            statement =connection.prepareStatement("select dname,loc from dept where  deptno = ?");
+               statement =  connection.createStatement();
+//           查询
+            resultSet = statement.executeQuery("select dname,deptno from dept where  deptno = 1000");
+            while (resultSet.next()){
+                int deptno = resultSet.getInt("deptno");
+                String dname = resultSet.getString("dname");
+                System.out.println(deptno+","+dname);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (null != resultSet){
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (null != statement){
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (null != connection){
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public static void main(String[] args) {
+        jdbcStep();
+    }
+}

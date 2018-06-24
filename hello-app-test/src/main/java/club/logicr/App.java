1a4f9c292c6b5dca079246161b97e0575c5b7fac
logicr <13314433445@163.com>
2018-06-24 21:27:14 +0800

    Maven practice code

---
+++ hello-app-test/src/main/java/club/logicr/App.java
@@ -0,0 +1,13 @@
+package club.logicr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "The Fucking World!" );
+    }
+}
---
+++ hello-app-test/src/test/java/club/logicr/AppTest.java
@@ -0,0 +1,20 @@
+package club.logicr;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}

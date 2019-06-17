946c8eb89ba98b3c1071a9b36435e782fc286640
logicr <13314433445@163.com>
2019-06-17 18:13:21 +0800

    study completed

--- day_6_17/src/main/java/MyClassTest.java
+++ day_6_17/src/main/java/MyClassTest.java
@@ -12,9 +12,51 @@ class MyTest {
     static {
         System.out.println("static");
     }
+
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+}
+
+class SonOfMy extends MyTest {
+    public SonOfMy() {
+        System.out.println("son construct");
+    }
+    {
+        System.out.println("son common");
+    }
+    static {
+        System.out.println("son Static ");
+    }
+
+    @Override
+    public void sayHello() {
+        super.sayHello();
+    }
+}
+
+class Out {
+    private String name ;
+
+    /**
+     * inner class
+     */
+    static class Inner {
+        public void changeName(String string) {
+//            System.out.println(name);
+//            Out.this.name = string;
+//            name = string;/*/
+//            System.out.println(name);
+            System.out.println(string);
+        }
+    }
 }
 public class MyClassTest {
     public static void main(String[] args) {
-        new MyTest();
+        new SonOfMy().sayHello();
+//        Out.Inner inner = new Out().new Inner();
+//        inner.changeName("Jan");
+        Out.Inner inner = new Out.Inner();
+        inner.changeName("s");
     }
 }

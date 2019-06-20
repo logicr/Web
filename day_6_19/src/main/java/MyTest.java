49cf448a49258619afd3c5312b4ed738acfff34f
logicr <13314433445@163.com>
2019-06-20 11:20:40 +0800

    supplement

--- day_6_19/src/main/java/MyTest.java
+++ day_6_19/src/main/java/MyTest.java
@@ -8,11 +8,17 @@ public class MyTest {
         String str2 = "Hello";
         String str3 = new String("Hello");
         String str4 = new String("Hello");
+        Object object = new String("Hello");
         System.out.println(str1 == str2);
         System.out.println(str3 == str4);
         System.out.println(str1 == str3);
         System.out.println(str1.equals(str3));
         System.out.println(str1.charAt(2));
         System.out.println(str1.startsWith("h"));
+        Integer num1 = new Integer(128);
+        Integer num2 = new Integer(128);
+        Integer num3 = 1;
+        Integer num4 = 1;
+        System.out.println(num4 == num3);
     }
 }
